c6aab4801a908accd09fea621cf15a21a05b1085
Christian J H <142556201+Christiajh@users.noreply.github.com>
2025-06-15 22:51:47 +0700

    d

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   plugins: [
     react(),
@@ -15,19 +15,19 @@ export default defineConfig(({ mode }) => ({
   ],
   server: {
     port: 5173,
-    proxy: mode === 'development' ? {
-      '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-        secure: false,
-      },
-    } : undefined,
+    proxy: mode === 'development'
+      ? {
+          '/api': {
+            target: 'http://localhost:3001', // Hanya digunakan saat development
+            changeOrigin: true,
+            secure: false,
+          },
+        }
+      : undefined,
   },
-  base: '/',
   build: {
     outDir: 'dist',
   },
-  // Tambahkan resolve fallback agar history mode berjalan saat development
   resolve: {
     alias: {
       '@': '/src',
